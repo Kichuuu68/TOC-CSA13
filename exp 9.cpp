cec9a81e9d803e6317f6b976c864ac068ed029f6
kishuu <113424403+Kichuuu68@users.noreply.github.com>
2023-08-11 13:49:21 +0530

    Update exp 9.cpp

--- exp 9.cpp
+++ exp 9.cpp
@@ -1,34 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-
-#define MAX_STRING_LENGTH 100
-
-int current_state = 0;
-
-int simulate_NFA(char* input_string) {
-    int length = strlen(input_string);
-    for (int i = 0; i < length; i++) {
-        if (current_state == 0 && input_string[i] == '0') {
-            current_state = 1;
-        } else if (current_state == 1 && input_string[i] == '1') {
-            current_state = 2;
-            return 1;
-        } else {
-            current_state = 0;
-            return 0;
-        }
-    }
-    return 0;
-}
-
-int main() {
-    char input_string[MAX_STRING_LENGTH];
-    printf("Enter a string to check: ");
-    scanf("%s", input_string);
-    if (simulate_NFA(input_string)) {
-        printf("Rejected\n");
-    } else {
-        printf("Accepted\n");
-    }
-    return 0;
+#include<stdio.h>
+#define max 100
+main() {
+   char str[max],f='a,b,c';
+   int i;
+   printf("enter the string to be checked: ");
+   scanf("%s",str);
+   for(i=0;str[i]!='\0';i++) {
+      switch(f) {
+         case 'a': if(str[i]=='0') f='b';
+            else if(str[i]=='1') f='a';
+         break;
+         case 'b': if(str[i]=='0') f='b';
+            else if(str[i]=='1') f='c';
+         break;
+         case 'c': if(str[i]=='0') f='b';
+            else if(str[i]=='1') f='a';
+         break;
+      }
+   }
+   if(f=='c')
+      printf("String is accepted", f);
+   else printf("String is not accepted", f);
+      return 0;
 }
